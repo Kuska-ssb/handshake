6173e50a53a6cda076c8e055ae51c70c7687251c
adria0 <adria@codecontext.io>
2019-11-18 17:29:37 +0100

    simpler whoami

--- code/examples/whoami.rs
+++ code/examples/whoami.rs
@@ -2,26 +2,27 @@ extern crate base64;
 extern crate code;
 extern crate crossbeam;
 
-use sodiumoxide::crypto::{auth, sign::ed25519};
-use std::io::{self};
+use std::io;
 use std::net::{TcpStream};
 
 use code::handshake::Handshake;
 use code::config::{IdentitySecret,ssb_net_id};
 use code::rpc;
 
-fn test_client(
-    socket: TcpStream,
-    net_id: auth::Key,
-    pk: ed25519::PublicKey,
-    sk: ed25519::SecretKey,
-    server_pk: ed25519::PublicKey,
-) -> io::Result<()> {
+fn main() -> io::Result<()> {
 
-    let handshake = Handshake::new_client(&socket, &socket, net_id, pk, sk)
+    env_logger::init();
+    log::set_max_level(log::LevelFilter::max());
+
+    let IdentitySecret{pk,sk,..} = IdentitySecret::from_local_config()
+        .expect("read local secret");
+
+    let socket = TcpStream::connect("127.0.0.1:8008")?;
+
+    let handshake = Handshake::new_client(&socket, &socket, ssb_net_id(), pk, sk)
         .send_client_hello()?
         .recv_server_hello()?
-        .send_client_auth(server_pk)?
+        .send_client_auth(pk)?
         .recv_server_accept()?;
 
     let (box_stream_read, box_stream_write) =
@@ -38,20 +39,6 @@ fn test_client(
         }
     }
     
-    println!("{}: {}",req_no,whoami.unwrap().id);
-    Ok(())
-}
-
-fn main() -> io::Result<()> {
-
-    env_logger::init();
-    log::set_max_level(log::LevelFilter::max());
-
-    let IdentitySecret{pk,sk,..} = IdentitySecret::from_local_config()
-        .expect("read local secret");
-
-    let socket = TcpStream::connect("127.0.0.1:8008")?;
-    test_client(socket, ssb_net_id(), pk, sk, pk)?;
-
+    println!("{}", whoami.unwrap().id);
     Ok(())
 }
