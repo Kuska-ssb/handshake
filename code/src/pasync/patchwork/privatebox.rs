03c501a1357451ff93b1c30b43c7a324d1351f19
adria0 <adria@codecontext.io>
2019-12-13 12:36:21 +0100

    nicer private box

--- code/src/pasync/patchwork/privatebox.rs
+++ code/src/pasync/patchwork/privatebox.rs
@@ -5,7 +5,15 @@ use sodiumoxide::crypto::sign::SecretKey;
 use sodiumoxide::crypto::{sign::ed25519, scalarmult::curve25519, secretbox};
 use async_std::io;
 
-const SUFFIX : &str = ".box";
+pub const SUFFIX : &str = ".box";
+
+pub const MAX_RECIPIENTS : u8 = 7;
+
+const RECIPIENT_COUNT_LEN : usize = 1;
+const ENCRYPTED_HEADER_LEN : usize = 
+    RECIPIENT_COUNT_LEN
+    + secretbox::KEYBYTES
+    + secretbox::MACBYTES;
 
 pub fn is_privatebox(text : &str) -> bool {
     text.ends_with(SUFFIX) 
@@ -45,7 +53,7 @@ fn cipher(plaintext : &[u8], recipients : &[&ed25519::PublicKey]) -> Result<Box<
         return Err(to_ioerr("message cannot be empty when encrypting"));
     }
 
-    if recipients.len() == 0 || recipients.len() > 7 {
+    if recipients.len() == 0 || recipients.len() > MAX_RECIPIENTS as usize{
         return Err(to_ioerr("bad number of recipients when encrypting"));
     }
 
@@ -62,19 +70,20 @@ fn cipher(plaintext : &[u8], recipients : &[&ed25519::PublicKey]) -> Result<Box<
     // The sender uses scalar multiplication to derive a shared secret for each recipient,
     //   and encrypts (number_of_recipents || y) for each one
     let h_sk_scalar = &h_sk.to_curve25519();
-    let mut plain_header = [0u8;33];
+    let mut plain_header = [0u8;RECIPIENT_COUNT_LEN+secretbox::KEYBYTES];
     plain_header[0]=recipients.len() as u8;
-    &plain_header[1..].copy_from_slice(&y[..]);
+    &plain_header[RECIPIENT_COUNT_LEN..].copy_from_slice(&y[..]);
 
     let mut buffer : Vec<u8> = Vec::with_capacity(   
-        24  // nonce
-        +32 // header public key
-        +49*recipients.len() // encrypted headers for each recipient
-        +16+plaintext.len() // tag + encrypted message contenth 
+        secretbox::NONCEBYTES  
+        + ed25519::PUBLICKEYBYTES 
+        + ENCRYPTED_HEADER_LEN*recipients.len() 
+        + secretbox::MACBYTES+plaintext.len() 
     );
 
     buffer.extend_from_slice(&nonce[..]);
     buffer.extend_from_slice(&h_pk.to_curve25519()[..]);
+
     for recipient in recipients {
         let key = curve25519::scalarmult(&h_sk_scalar, &recipient.to_curve25519())
             .map_err(|_| to_ioerr("scalarmult failed"))?;
@@ -85,28 +94,30 @@ fn cipher(plaintext : &[u8], recipients : &[&ed25519::PublicKey]) -> Result<Box<
         buffer.extend_from_slice(&secretbox::seal(&plain_header[..],&nonce, &key));
     }
     buffer.extend_from_slice(&cipher_message[..]);
+
     Ok(buffer.into_boxed_slice())
 }
 
 fn decipher(ciphertext : &[u8], sk : &SecretKey) -> Result<Option<Vec<u8>>,io::Error> {
 
-    let nonce = secretbox::Nonce::from_slice(&ciphertext[..secretbox::NONCEBYTES])
+    let mut cursor = ciphertext;
+    
+    let nonce = secretbox::Nonce::from_slice(&cursor[..secretbox::NONCEBYTES])
         .ok_or(to_ioerr("cannot read nonce"))?;
+    cursor = &cursor[secretbox::NONCEBYTES..];
 
-    let h_pk = curve25519::GroupElement::from_slice(&ciphertext[secretbox::NONCEBYTES..secretbox::NONCEBYTES+32])
+    let h_pk = curve25519::GroupElement::from_slice(&cursor[..ed25519::PUBLICKEYBYTES])
         .ok_or(to_ioerr("cannot convert to curve"))?;
+    cursor = &cursor[ed25519::PUBLICKEYBYTES..];
     
     let key = curve25519::scalarmult(&sk.to_curve25519(), &h_pk)
-        .map_err(|_| to_ioerr("cannot scalarmult"))?;
+        .and_then(|key| secretbox::Key::from_slice(&key[..]).ok_or(()))
+        .map_err(|_| to_ioerr("cannot create key"))?;
 
-    let key = secretbox::Key::from_slice(&key[..])
-        .ok_or(to_ioerr("cannot create key"))?;
-
-    let mut cursor = &ciphertext[secretbox::NONCEBYTES+32..];
     let mut header_no = 0;
-    while header_no < 7 && cursor.len() >= 49+17 {
-        if let Ok(header) = secretbox::open(&cursor[..49], &nonce, &key) {
-            let encrypted_message_offset = 49*(header[0]-header_no) as usize;
+    while header_no < MAX_RECIPIENTS && cursor.len() >= ENCRYPTED_HEADER_LEN+secretbox::MACBYTES+1 {
+        if let Ok(header) = secretbox::open(&cursor[..ENCRYPTED_HEADER_LEN], &nonce, &key) {
+            let encrypted_message_offset = ENCRYPTED_HEADER_LEN*(header[0]-header_no) as usize;
             let y =  secretbox::Key::from_slice(&header[1..])
                 .ok_or(to_ioerr("cannot create key"))?;
             let plaintext = secretbox::open(&cursor[encrypted_message_offset..], &nonce, &y)
