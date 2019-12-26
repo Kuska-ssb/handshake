48980aa3bdd4cc9fe181187f15df4d3de7966fc4
adria0 <adria@codecontext.io>
2019-12-26 23:57:22 +0100

    Debug -> ToString for generic error map

--- code/src/pasync/util/error.rs
+++ code/src/pasync/util/error.rs
@@ -1,6 +1,7 @@
 use async_std::io;
+use std::fmt::Debug;
 
-pub fn to_ioerr<T: ToString>(err: T) -> io::Error {
-    io::Error::new(io::ErrorKind::Other, err.to_string())
+pub fn to_ioerr<T: Debug>(err: T) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, format!("{:?}",err))
 }
 
