73224fea27f597de859a925c4996dc8657b86d40
mycognosist <gnomad@cryptolab.net>
2023-10-17 08:59:08 +0200

    remove mutable variable assignments to satisfy clippy

--- src/sync/handshake.rs
+++ src/sync/handshake.rs
@@ -31,19 +31,19 @@ pub fn handshake_client<T: Read + Write>(
     let mut buf = [0; 128];
     let handshake = Handshake::new_client(net_id, pk, sk);
 
-    let mut send_buf = &mut buf[..handshake.send_bytes()];
+    let send_buf = &mut buf[..handshake.send_bytes()];
     let handshake = handshake.send_client_hello(send_buf);
     stream.write_all(send_buf)?;
 
-    let mut recv_buf = &mut buf[..handshake.recv_bytes()];
+    let recv_buf = &mut buf[..handshake.recv_bytes()];
     stream.read_exact(recv_buf)?;
     let handshake = handshake.recv_server_hello(recv_buf)?;
 
-    let mut send_buf = &mut buf[..handshake.send_bytes()];
+    let send_buf = &mut buf[..handshake.send_bytes()];
     let handshake = handshake.send_client_auth(send_buf, server_pk)?;
     stream.write_all(send_buf)?;
 
-    let mut recv_buf = &mut buf[..handshake.recv_bytes()];
+    let recv_buf = &mut buf[..handshake.recv_bytes()];
     stream.read_exact(recv_buf)?;
     let handshake = handshake.recv_server_accept(recv_buf)?;
 
@@ -59,19 +59,19 @@ pub fn handshake_server<T: Read + Write>(
     let mut buf = [0; 128];
     let handshake = Handshake::new_server(net_id, pk, sk);
 
-    let mut recv_buf = &mut buf[..handshake.recv_bytes()];
+    let recv_buf = &mut buf[..handshake.recv_bytes()];
     stream.read_exact(recv_buf)?;
     let handshake = handshake.recv_client_hello(recv_buf)?;
 
-    let mut send_buf = &mut buf[..handshake.send_bytes()];
+    let send_buf = &mut buf[..handshake.send_bytes()];
     let handshake = handshake.send_server_hello(send_buf);
     stream.write_all(send_buf)?;
 
-    let mut recv_buf = &mut buf[..handshake.recv_bytes()];
+    let recv_buf = &mut buf[..handshake.recv_bytes()];
     stream.read_exact(recv_buf)?;
     let handshake = handshake.recv_client_auth(recv_buf)?;
 
-    let mut send_buf = &mut buf[..handshake.send_bytes()];
+    let send_buf = &mut buf[..handshake.send_bytes()];
     let handshake = handshake.send_server_accept(send_buf);
     stream.write_all(send_buf)?;
 
